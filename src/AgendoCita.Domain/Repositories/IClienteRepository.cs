15f9dce4e81a208a42933769c194093b48025ccd
Pablo ORIOLI <pablo_orioli@aysa.com.ar>
2026-05-04 10:14:09 -0300

    feat: interfaces

---
+++ src/AgendoCita.Domain/Repositories/IClienteRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using AgendoCita.Domain.Entities;
+namespace AgendoCita.Domain.Repositories;
+
+public interface IClienteRepository : IRepository<Cliente>
+{
+    Task<Cliente?> GetByEmailAsync(string email);
+    Task<IEnumerable<Cliente>> GetByProfesionalIdAsync(int profesionalId);
+}
---
+++ src/AgendoCita.Domain/Repositories/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgendoCita.Domain.Repositories;
+
+public interface IUnitOfWork : IDisposable
+{
+    IClienteRepository Clientes { get; }
+    //Task<int> SaveChangesAsync();   
+    Task<int> CompleteAsync();
+}
